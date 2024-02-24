288afd88ee15d66e26b61f41e618bc230cf55cf6
Jhenil Parihar <jhenilparihar@gmail.com>
2024-02-24 10:42:43 +0530

    Initial commit

---
+++ src/utilities/ApiService.js
@@ -0,0 +1,56 @@
+const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+const WEATHER_API_KEY = "77e016036f83e91f71ed53de6090ee03";
+
+const GEO_API_OPTIONS = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4f0dcce84bmshac9e329bd55fd14p17ec6fjsnff18c2e61917",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export async function getCity(lat, lon) {
+  // const response = await fetch(
+  //   `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${lat}${lon}/nearbyCities?limit=1`,
+  //   GEO_API_OPTIONS
+  // );
+  // const data = await response.json();
+  // console.log(data);
+  // const city = extractCityFromResponse(data);
+  return "Mumbai";
+}
+
+export async function fetchWeatherData(lat, lon) {
+  try {
+    let [weatherPromise, forcastPromise] = await Promise.all([
+      fetch(
+        `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+      fetch(
+        `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+    ]);
+
+    const weatherResponse = await weatherPromise.json();
+    const forcastResponse = await forcastPromise.json();
+    return [weatherResponse, forcastResponse];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchCities(input) {
+  try {
+    const response = await fetch(
+      `${GEO_API_URL}/cities?minPopulation=10000&namePrefix=${input}`,
+      GEO_API_OPTIONS
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
